c380697e15afe68dbc9b9bada8c8708ecabf2523
abubakarsiddique10 <abubakarsiddique7912@gmail.com>
2022-03-02 22:34:56 +0600

    added spinner

--- js/main.js
+++ js/main.js
@@ -1,23 +1,33 @@
 // get mobile data from server
 const loadMobilePhones = () => {
-    const inputId = document.getElementById('search-field').value;
-    const inputText = inputId.toLowerCase();
-    fetch(`https://openapi.programming-hero.com/api/phones?search=${inputText}`)
-        .then(res => res.json())
-        .then(data => showMobilePhones(data.data))
+    const inputId = document.getElementById('search-field');
+    const inputText = inputId.value.toLowerCase();
+    inputId.value = '';
+    document.getElementById('spinner').style.display = 'block';
+    if (isNaN(inputText)) {
+        fetch(`https://openapi.programming-hero.com/api/phones?search=${inputText}`)
+            .then(res => res.json())
+            .then(data => showMobilePhones(data.data))
+    }
+    else {
+        document.getElementById('error').style.display = 'block';
+        document.getElementById('spinner').style.display = 'none';
+    }
 }
 // show mobile data in website
 const showMobilePhones = (getAllPhone) => {
-    const phones = getAllPhone.slice(0, 3)
-    /*  console.log(phones) */
-    const addColumn = document.getElementById('add-column');
-    addColumn.textContent = '';
-
-    phones.forEach(phone => {
-        /*  console.log(phone); */
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
+    if (getAllPhone.length == 0) {
+        document.getElementById('error').style.display = 'block';
+    }
+    else {
+        document.getElementById('error').style.display = 'none'
+        const phones = getAllPhone.slice(0, 20);
+        const addColumn = document.getElementById('add-column');
+        addColumn.textContent = '';
+        phones.forEach(phone => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
             <div class="col text-center ">
                 <div class="card h-100 border-0 py-5">
                     <img class="w-50 mx-auto" src="${phone.image}" class="card-img-top" alt="...">
@@ -26,13 +36,13 @@ const showMobilePhones = (getAllPhone) => {
                         <p class="card-text fw-bold fs-5">${phone.brand}</p>
                         <button onclick="getPhoneDetails('${phone.slug}')" class="py-2 px-5 rounded border-0">Show Details</button>
                     </div>
-                   
                 </div>
             </div>
         `;
-        addColumn.appendChild(div);
-        getPhoneDetails(`${phone.slug}`)
-    })
+            addColumn.appendChild(div);
+        })
+    }
+    document.getElementById('spinner').style.display = 'none';
 }
 // get mobile phone details from server
 const getPhoneDetails = (slug) => {
@@ -43,23 +53,27 @@ const getPhoneDetails = (slug) => {
 }
 // display every moblie phone details
 const displayDeatails = (phone) => {
-    console.log(phone)
+    /* console.log(phone) */
     const showDetails = document.getElementById('show-details');
     showDetails.textContent = '';
     const div1 = document.createElement('div');
-    div1.classList.add('col-5');
+    div1.classList.add('col-lg-5');
+    div1.classList.add('col-12');
     div1.innerHTML = `
         <div class="card h-100 text-center py-5">
             <img class="w-50 mx-auto" src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body pb-0">
                 <h5 class="fw-bold fs-4">${phone.brand}</h5>
                 <h5 class="card-title">${phone.name}</h5>
-                <h6 class="mb-0">${phone.releaseDate ? phone.releaseDate : 'No release date found'}</h6>
+                <h6 class="mb-4">${phone.releaseDate ? phone.releaseDate : 'No release date found'}</h6>
+                <button style="background: #2ABBE8" class="py-2 px-5 border-0 mb-3">Bye Now</button>
+                <button class="py-2 px-5 border-0">Add to Cart</button>
             </div>
         </div>
     `;
     const div2 = document.createElement('div');
-    div2.classList.add('col-7');
+    div2.classList.add('col-lg-7');
+    div2.classList.add('col-12')
     div2.innerHTML = `
         <div class="card h-100">
             <div class="card-body p-4">
@@ -85,6 +99,8 @@ const displayDeatails = (phone) => {
                             <li>${phone.mainFeatures.sensors[3]}</li>
                             <li>${phone.mainFeatures.sensors[4]}</li>
                             <li>${phone.mainFeatures.sensors[5]}</li>
+                            <li>${phone.mainFeatures.sensors[6]}</li>
+                            <li>${phone.mainFeatures.sensors[7]}</li>
                         </ul>
                     </div>
                 </div>
@@ -92,12 +108,12 @@ const displayDeatails = (phone) => {
                     <h5 class="card-title">Others</h5>
                     <div>
                         <ul>
-                            <li><span class="fw-bold me-2">Bluetooth :</span>${phone?.others?.Bluetooth}</li>
-                            <li><span class="fw-bold me-2">GPS  :</span>${phone?.others?.GPS}</li>
-                            <li><span class="fw-bold me-2">Bluetooth :</span>${phone?.others?.NFC}</li>
-                            <li><span class="fw-bold me-2">GPS  :</span>${phone?.others?.Radio}</li>
-                            <li><span class="fw-bold me-2">Bluetooth :</span>${phone?.others?.USB}</li>
-                            <li><span class="fw-bold me-2">GPS  :</span>${phone?.others?.WLAN}</li>
+                            <li><span class="fw-bold me-2">Bluetooth :</span>${phone.others ? phone.others.Bluetooth : 'No'}</li>
+                            <li><span class="fw-bold me-2">GPS  :</span>${phone.others ? phone.others.GPS : 'No'}</li>
+                            <li><span class="fw-bold me-2">NFC :</span>${phone.others ? phone.others.NFC : 'No'}</li>
+                            <li><span class="fw-bold me-2">Radio  :</span>${phone.others ? phone.others.Radio : 'No'}</li>
+                            <li><span class="fw-bold me-2">USB :</span>${phone.others ? phone.others.USB : 'No'}</li>
+                            <li><span class="fw-bold me-2">WLAN  :</span>${phone.others ? phone.others.WLAN : 'No'}</li>
                         </ul>
                     </div>
                 </div>
